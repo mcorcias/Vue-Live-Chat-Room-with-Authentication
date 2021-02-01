c437d94dd7ef9a37000366251eea9e361ee8e560
mcorcias <mosescorcias@gmail.com>
2021-02-01 20:44:58 +0200

    make 2 components for login and signup

---
+++ live-chat/src/components/SignupForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      <input type="text" required placeholder="display name" v-model="displayname">
+      <input type="email" required placeholder="email" v-model="email">
+      <input type="password" required placeholder="password" v-model="password">
+      <button>Sign Up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    setup(){
+        //refs
+        const displayname=ref('')
+        const email=ref('')
+        const password=ref('')
+
+        const handleSubmit=()=>{
+            console.log(displayname.value,email.value,password.value);
+        }
+
+        return {displayname,email,password,handleSubmit}
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ live-chat/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAFxAa16iHQJsuLN1esAT8pU9Pu095zDnM',
+  authDomain: 'blog-posts-a2c0f.firebaseapp.com',
+  projectId: 'blog-posts-a2c0f',
+  storageBucket: 'blog-posts-a2c0f.appspot.com',
+  messagingSenderId: '148961775521',
+  appId: '1:148961775521:web:d8a923cd9ebfe240e9c5e0',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, timestamp };
