4f00505faa2bd69c011a860bad39c4cc2cd07f3f
mcorcias <mosescorcias@gmail.com>
2021-02-01 23:15:39 +0200

    created Navbar component and nested inside Welcome component

--- live-chat/src/views/Chatroom.vue
+++ live-chat/src/views/Chatroom.vue
@@ -1,10 +1,13 @@
 <template>
-  <p>Chatroom</p>
+  <div class="container">
+      <Navbar/>
+  </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar'
 export default {
-
+    components:{Navbar}
 }
 </script>
 
