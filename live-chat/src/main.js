fc3403d85b91c9153f7bbef9e5e097f6b0dbdd74
mcorcias <mosescorcias@gmail.com>
2021-02-02 21:40:17 +0200

    Prevent the user to get access to chatroom page when he's not Auth - if the user has an access and he make refresh when he's inside the chatroom screen its redirect to welcome this is problem - to fix it we mount the app only after we get information about the user auth from the firebase  - look in main.js

--- live-chat/src/main.js
+++ live-chat/src/main.js
@@ -2,7 +2,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/main.css';
+import { projectAuth } from './firebase/config';
 
-createApp(App)
-  .use(router)
-  .mount('#app');
+let app;
+
+// Waiting for Firebase Auth to initialize and update the user state
+projectAuth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App)
+      .use(router)
+      .mount('#app');
+  }
+});
