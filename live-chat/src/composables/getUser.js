13481c17a359323dccc2ad3fd085b4b625c47bf4
mcorcias <mosescorcias@gmail.com>
2021-02-02 23:00:18 +0200

    created a new compnent NewChatForm  - nested inside ChatRoom Component

--- live-chat/src/composables/getUser.js
+++ live-chat/src/composables/getUser.js
@@ -6,7 +6,6 @@ const user = ref(null);
 // Listen to user state - If he loggedIn, logedOut ,singUp
 // If the user loggedOut the user will be null
 projectAuth.onAuthStateChanged(_user => {
-  console.log('User state change. Current user is: ', _user);
   user.value = _user;
 });
 
--- live-chat/src/views/Chatroom.vue
+++ live-chat/src/views/Chatroom.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="container">
       <Navbar/>
+      <NewChatForm/>
   </div>
 </template>
 
 <script>
+import NewChatForm from '../components/NewChatForm'
 import Navbar from '../components/Navbar'
 import getUser from '../composables/getUser'
 import { useRouter } from 'vue-router'
 import { watchEffect} from 'vue'
 export default {
-    components:{Navbar},
-    setup(props,context){
+    components:{Navbar,NewChatForm},
+    setup(){
       const router = useRouter()
 
       const {user}= getUser()
