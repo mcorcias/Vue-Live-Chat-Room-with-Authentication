48f75ea9dd4966c7337006cc81bdc04366d99b17
mcorcias <mosescorcias@gmail.com>
2021-02-02 22:19:29 +0200

    watch after the user state in the Chatroom component  - If the user.value is null its mean he has been logged out, so the watcherEffect recognize it and redirect the user to the welcome page

--- live-chat/src/views/Chatroom.vue
+++ live-chat/src/views/Chatroom.vue
@@ -6,8 +6,25 @@
 
 <script>
 import Navbar from '../components/Navbar'
+import getUser from '../composables/getUser'
+import { useRouter } from 'vue-router'
+import { watchEffect} from 'vue'
 export default {
-    components:{Navbar}
+    components:{Navbar},
+    setup(props,context){
+      const router = useRouter()
+
+      const {user}= getUser()
+
+      watchEffect(()=>{
+        if (!user.value){
+          router.push({name:'Welcome'})
+        }
+      })
+
+
+      return{user}
+    }
 }
 </script>
 
