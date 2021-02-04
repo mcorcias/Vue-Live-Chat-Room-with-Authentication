a6defc4281212e55641252c54247e0803fa25567
mcorcias <mosescorcias@gmail.com>
2021-02-04 22:13:47 +0200

    If the user auth there is no point to see again the welcom screen

--- live-chat/src/router/index.js
+++ live-chat/src/router/index.js
@@ -14,11 +14,23 @@ const requireAuth = (to, from, next) => {
   }
 };
 
+// If the user auth there is no point to see again the welcom screen
+const requireNoAuth = (to, from, next) => {
+  let user = projectAuth.currentUser;
+  console.log('current user in auth guard:', user);
+  if (user) {
+    next({ name: 'Chatroom' });
+  } else {
+    next();
+  }
+};
+
 const routes = [
   {
     path: '/',
     name: 'Welcome',
     component: Welcome,
+    beforeEnter: requireNoAuth,
   },
   {
     path: '/chatroom',
